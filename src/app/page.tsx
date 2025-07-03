5081e2824927ca1499bc6bd2a4a2f34685fe3aaf
Mansour13737 <mansournosrati69@gmail.com>
2025-07-03 19:03:44 +0330

    4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
     <div className="w-full relative flex flex-col min-h-screen bg-[url('/Home1.png')] lazy bg-no-repeat bg-cover bg-center">
       <Navbar />
       <p className="text-[#be9858] font-display text-center text-[25px] font-light mt-6">JEWELRY & WATCH <br /><span className="text-[40px]">PHOTOGRAPHY</span></p>
-      <span className="text-white/90 tracking-wider absolute bottom-29 font-display text-center text-[14px] left-3 font-light">GET IN TOUCH</span>
-      <ul className="flex gap-2 absolute left-3 bottom-21">
+      <span className="text-white/90 tracking-wider absolute bottom-32 font-display text-center text-[14px] left-3 font-light">GET IN TOUCH</span>
+      <ul className="flex gap-2 absolute left-3 bottom-24">
         <Link href={'tel:+989120687097'}>
           <Image src={'/call.svg'} width={24} height={24} alt='pic' loading="lazy"/>
         </Link>
@@ -20,7 +20,7 @@ export default function Home() {
           <Image src={'/instagram.svg'} width={24} height={24} alt='pic' loading="lazy"/>
         </Link>
       </ul>
-      <Button hrf="/gallery" cls='border-[0.5px] absolute border-white/80 text-white/90 bottom-15 right-15 w-fit py-2 px-10 font-display tracking-widest rounded-[3px] text-[14px] backdrop-blur-[3px]'>
+      <Button hrf="/gallery" cls='border-[0.5px] absolute border-white/80 text-white/90 bottom-18 right-15 w-fit py-2 px-10 font-display tracking-widest rounded-[3px] text-[14px] backdrop-blur-[3px]'>
         GALLERY
       </Button>
     </div>
