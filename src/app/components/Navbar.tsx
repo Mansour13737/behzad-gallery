e590b4c4ee87692f0988145d295f511afa805811
Mansour13737 <mansournosrati69@gmail.com>
2025-07-06 11:26:44 +0330

    7

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div className="w-full px-4 py-4 font-display border-b-white/60 border-[0.5px]">
+    <div className="w-full px-4 py-3 font-display border-b-white/60 border-[0.5px]">
       <ul className="flex gap-5 text-[#be9858] text-[14px]">
         <Link href={'/gallery'}>GALLERY</Link>
         <Link href={'/about'}>ABOUT</Link>
