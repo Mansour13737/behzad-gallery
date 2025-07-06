9ea8036083e922a1eb18dc0b29823770ff4954aa
Mansour13737 <mansournosrati69@gmail.com>
2025-07-06 11:34:21 +0330

    7

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,10 +4,10 @@ import Navbar from "../components/Navbar";
 
 export default function Contact() {
   return (
-    <div className="min-h-screen relative flex flex-col bg-[url('/Home1.png')] bg-no-repeat bg-cover bg-center px-6 py-16 md:px-20 text-white font-display font-light">
+    <div className="min-h-screen relative w-full flex flex-col bg-[url('/Home1.png')] bg-no-repeat bg-cover bg-center md:px-20 text-white font-display font-light">
       <Navbar />
 
-      <div className="max-w-3xl mx-auto bg-black/60 backdrop-blur-md rounded-lg p-4 rounded-[8px] flex flex-col items-center shadow-lg">
+      <div className="max-w-[90%] mx-auto bg-black/60 backdrop-blur-md rounded-lg px-5 py-6 rounded-[8px] flex flex-col items-center shadow-lg">
         <h1 className="text-[#be9858] text-4xl mb-4 text-center tracking-wide">Get In Touch</h1>
         <p className="text-white/90 mb-8 text-left leading-relaxed">
           Whether you have a project in mind, want to collaborate, or just say hello,
@@ -15,7 +15,7 @@ export default function Contact() {
         </p>
 
 
-        <div className="mt-12 text-left text-white/70 space-y-2 font-light text-sm self-start">
+        <div className="mt-[30%] text-left text-white/70 space-y-2 font-light text-sm self-start">
           <p>📩 <a href="mailto:behbiin@gmail.com" className="underline hover:text-[#be9858]">behbiin@gmail.com</a></p>
           <p>📞 <a href="tel:+989120687097" className="underline hover:text-[#be9858]">+98 912 068 7097</a></p>
           <p>
@@ -26,9 +26,7 @@ export default function Contact() {
         </div>
       </div>
 
-      <span className="text-[#be9858] tracking-wider absolute bottom-20 left-6 font-display text-[14px] font-light">
-        GET IN TOUCH
-      </span>
+
     </div>
   );
 }
